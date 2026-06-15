363300c39e81e8273b52b1e21cfaa592e2ef5cb3
Harry Lew <harrylew25@gmail.com>
2026-06-16 01:04:13 +0800

    feature: fetch handle tutorial with basic effect api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,32 @@
-import { Console, Effect } from 'effect';
+import { Effect, Data } from 'effect';
 
-const main = Console.log('Hello, World!');
+class FetchError extends Data.TaggedError('FetchError')<{}> {}
 
-Effect.runSync(main);
+class JsonError extends Data.TaggedError('JsonError')<{}> {}
+
+const fetchRequest = (pokemon: string) =>
+  Effect.tryPromise({
+    try: () => fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`),
+    catch: () => new FetchError(),
+  });
+
+const jsonResponse = (response: Response) =>
+  Effect.tryPromise({
+    try: () => response.json(),
+    catch: () => new JsonError(),
+  });
+
+const main = (pokemonName: string) =>
+  fetchRequest(pokemonName).pipe(
+    Effect.filterOrFail(
+      (response) => response.ok,
+      () => new FetchError(),
+    ),
+    Effect.flatMap(jsonResponse),
+    Effect.catchTags({
+      FetchError: () => Effect.succeed('Failed to fetch the data.'),
+      JsonError: () => Effect.succeed('Failed to parse the JSON.'),
+    }),
+  );
+
+Effect.runPromise(main('ditto')).then(console.log);
