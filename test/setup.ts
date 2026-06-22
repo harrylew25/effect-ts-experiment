903bc9daed41cc860259c7c33928880dffa8c2fc
Harry Lew <harrylew25@gmail.com>
2026-06-22 19:27:07 +0800

    feature: setup unit test and mock with vitest and msw

---
+++ test/setup.ts
@@ -0,0 +1,10 @@
+import { afterAll, afterEach, beforeAll } from 'vitest';
+import { server } from '../test/node';
+
+beforeAll(() =>
+	server.listen({
+		onUnhandledRequest: 'error',
+	}),
+);
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
