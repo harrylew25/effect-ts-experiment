084c214255cab0912170bce33e263c125c327835
Harry Lew <harrylew25@gmail.com>
2026-06-22 19:27:28 +0800

    feature: added basic unit test

---
+++ test/pokeApiUrl.test.ts
@@ -0,0 +1,31 @@
+import { ConfigProvider, Effect, Layer } from 'effect';
+import { expect, it } from 'vitest';
+import { PokeApiServiceTag } from '../src/PokeApi';
+
+const TestConfigProvider = ConfigProvider.fromMap(
+	new Map([['BASE_URL', 'http://localhost:3000']]),
+);
+
+const ConfigProviderLayer = Layer.setConfigProvider(TestConfigProvider);
+const MainLayer = PokeApiServiceTag.Default.pipe(
+	// Provide the Config Provider layer to PokeApiServiceTag.Live
+	Layer.provide(ConfigProviderLayer),
+);
+
+const program = Effect.gen(function* () {
+	const pokeApi = yield* PokeApiServiceTag;
+	return yield* pokeApi.getPokemon;
+});
+
+const main = program.pipe(Effect.provide(MainLayer));
+
+it('returns a valid pokemon', async () => {
+	const response = await Effect.runPromise(main);
+	expect(response).toEqual({
+		id: 1,
+		height: 10,
+		weight: 10,
+		order: 1,
+		name: 'bulbasaur',
+	});
+});
