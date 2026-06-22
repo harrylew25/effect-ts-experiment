80741e8c8c4e8f8eb5d4370b1df604ad7cc17d98
Harry Lew <harrylew25@gmail.com>
2026-06-23 00:43:09 +0800

    feature: adding runtime api into the code

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-import { Effect, Layer } from 'effect';
+import { Effect, Layer, ManagedRuntime } from 'effect';
 import { PokeApiServiceTag } from './PokeApi';
 
 // if development, use mock layer, otherwise use live layer
 
 const MainLayer = Layer.mergeAll(PokeApiServiceTag.Default);
 
-const program = Effect.gen(function* () {
+const PokemonRuntime = ManagedRuntime.make(MainLayer);
+
+export const program = Effect.gen(function* () {
 	const pokeApi = yield* PokeApiServiceTag;
 	return yield* pokeApi.getPokemon;
 });
 
-const runnable = program.pipe(Effect.provide(MainLayer));
+// const runnable = program.pipe(Effect.provide(MainLayer));
 
-const main = runnable.pipe(
+const main = program.pipe(
 	Effect.catchTags({
 		FetchError: () => Effect.succeed('Fetch error'),
 		JsonError: () => Effect.succeed('JSON error'),
@@ -20,4 +22,5 @@ const main = runnable.pipe(
 	}),
 );
 
-Effect.runPromise(main).then(console.log);
+// Effect.runPromise(main).then(console.log);
+PokemonRuntime.runPromise(main).then(console.log);
--- test/pokeApiUrl.test.ts
+++ test/pokeApiUrl.test.ts
@@ -1,4 +1,4 @@
-import { ConfigProvider, Effect, Layer } from 'effect';
+import { ConfigProvider, Effect, Layer, ManagedRuntime } from 'effect';
 import { expect, it } from 'vitest';
 import { PokeApiServiceTag } from '../src/PokeApi';
 
@@ -12,6 +12,8 @@ const MainLayer = PokeApiServiceTag.Default.pipe(
 	Layer.provide(ConfigProviderLayer),
 );
 
+const TestingRuntime = ManagedRuntime.make(MainLayer);
+
 const program = Effect.gen(function* () {
 	const pokeApi = yield* PokeApiServiceTag;
 	return yield* pokeApi.getPokemon;
@@ -20,7 +22,7 @@ const program = Effect.gen(function* () {
 const main = program.pipe(Effect.provide(MainLayer));
 
 it('returns a valid pokemon', async () => {
-	const response = await Effect.runPromise(main);
+	const response = await TestingRuntime.runPromise(program);
 	expect(response).toEqual({
 		id: 1,
 		height: 10,
